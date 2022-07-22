77e8d5744f6decd6d54439b38821e432f0b277b0
Emil Haugbergsmyr <emil@raeven.net>
2022-07-22 20:43:28 +0200

    Added tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,2 +1,69 @@
+use crate::{Database, Entry};
 
+#[derive(Debug, Clone, Entry, PartialEq)]
+pub struct ItemOne {
+    pub id: u32,
+}
 
+impl ItemOne {
+    pub fn new(id: u32) -> Self {
+        Self { id }
+    }
+}
+
+#[derive(Debug, Clone, Entry, PartialEq)]
+pub struct ItemTwo {
+    pub id: u32,
+}
+
+impl ItemTwo {
+    pub fn new(id: u32) -> Self {
+        Self { id }
+    }
+}
+
+#[test]
+fn count_inserted_items() {
+    let mut db = Database::default();
+
+    db.insert(ItemOne::new(1));
+    db.insert(ItemOne::new(2));
+    db.insert(ItemTwo::new(1));
+
+    assert_eq!(db.count::<ItemOne, _>(|_| true), 2);
+    assert_eq!(db.count::<ItemTwo, _>(|_| true), 1);
+}
+
+#[test]
+fn get_inserted_items() {
+    let mut db = Database::default();
+
+    db.insert(ItemOne::new(1));
+    db.insert(ItemTwo::new(1));
+    db.insert(ItemTwo::new(2));
+
+    assert_eq!(
+        db.get::<ItemOne, _>(|i| i.id == 1).unwrap()[0],
+        ItemOne::new(1)
+    );
+    assert_eq!(
+        db.get::<ItemTwo, _>(|i| i.id == 1).unwrap()[0],
+        ItemTwo::new(1)
+    );
+    assert_eq!(
+        db.get::<ItemTwo, _>(|i| i.id == 2).unwrap()[0],
+        ItemTwo::new(2)
+    );
+}
+
+#[test]
+fn verify_counters() {
+    let mut db = Database::default();
+
+    db.insert(ItemOne::new(1));
+    db.insert(ItemTwo::new(1));
+    db.insert(ItemOne::new(2));
+
+    assert_eq!(db.counter::<ItemOne>(), 2);
+    assert_eq!(db.counter::<ItemTwo>(), 1);
+}
